173696e404953cea51c4e004b5331d0823ad4198
プロソニー <ProsonyOriginal@yandex.ru>
2019-04-28 19:47:12 +0300

    init commit

--- core/router/router.js
+++
@@ -1,7 +0,0 @@
-import  { Router }      from 'express';
-import  authentication  from './routes/authentication';
-import  session         from './routes/session';
-
-export  default Router()
-        .use(session)
-        .use(`/authentication`, authentication)
